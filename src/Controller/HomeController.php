f216a6670cada0a40ca436412f8430eaf1586b35
Elwan <144356501+ElwanBeauchot@users.noreply.github.com>
2025-01-09 20:53:18 +0100

    [STORY-2] add read book in db

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\BookRead;
+use App\Form\AddReadBookType;
 use App\Repository\BookReadRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -19,19 +23,39 @@ class HomeController extends AbstractController
     }
 
     #[Route('/', name: 'app.home')]
-    public function index(): Response
+    public function index(Request $request, EntityManagerInterface $entityManager): Response
     {
         if($this->getUser() == null){
             return $this->redirectToRoute('auth.login');
         }
-
         $userId = $this->getUser()->getId();
         $booksRead  = $this->bookReadRepository->findByUserId($userId, false);
+
+        $bookRead = new BookRead();
+        $form = $this->createForm(AddReadBookType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            if ($bookRead->isRead() === null) {
+                $bookRead->setIsRead(false);
+            }
+            $bookRead->setRating($form->get('rating')->getData());
+            $bookRead->setDescription($form->get('description')->getData());
+            $bookRead->setUserId($userId);
+            $bookRead->setBookId($form->get('book_id')->getData());
+            $bookRead->setCreatedAt(new \DateTime());
+            $bookRead->setUpdatedAt(new \DateTime());
+            $entityManager->persist($bookRead);
+            $entityManager->flush();
+        }
+
+
 
         return $this->render('pages/home.html.twig', [
             'booksRead' => $booksRead,
             'userId'      => $userId,
             'name'      => 'Accueil', // Pass data to the view
+            'AddReadBookForm' => $form,
         ]);
     }
 
